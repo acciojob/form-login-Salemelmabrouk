2d8a4f570a2c36d0c0f429415bf8f7a8d59d3d5b
Salemelmabrouk <149633706+Salemelmabrouk@users.noreply.github.com>
2024-02-28 21:03:08 +0100

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
    function getFormValue() {
      
     var form = document.getElementById("myForm"); 
-    var firstName = form.elements["firstName"].value;
-    var lastName = form.elements["lastName"].value;
+    var firstName = form.elements["fname"].value;
+    var lastName = form.elements["lname"].value;
  
     var fullName = firstName + "\n" + lastName;
 
